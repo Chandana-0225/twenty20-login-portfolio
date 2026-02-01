7a1bb00483eb88fa7c55d72ef39ae473659d9dc0
Chandana B S <bschandana0225@gmail.com>
2026-02-01 09:59:47 +0530

    Fix static paths for JS and CSS

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.post("/register", (req, res) => {
   res.json({ message: "Registration successful (demo)" });
 });
 
 app.post("/login", (req, res) => {
   res.json({ success: true });
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 module.exports = app;
